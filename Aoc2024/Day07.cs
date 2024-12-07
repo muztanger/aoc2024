c5ca52d8fb522fe40470d423456bf419ce3b0073
Johan Östrand <muztanger@gmail.com>
2024-12-07 07:00:06 +0100

    Day7 probably missinterpreted part 2

--- Aoc2024/Day07.cs
+++ Aoc2024/Day07.cs
@@ -1,3 +1,5 @@
+using System.Security.Cryptography;
+
 namespace Advent_of_Code_2024;
 
 [TestClass]
@@ -30,7 +32,7 @@ public class Day07
                 }
                 visited.Add(current);
 
-                if (current.Length == numbers.Count)
+                if (current.Length == numbers.Count - 1)
                 {
                     var sum = numbers[0];
                     for (var i = 1; i < numbers.Count; i++)
@@ -54,7 +56,11 @@ public class Day07
     
     private static string Part2(IEnumerable<string> input)
     {
-        var operations = new Dictionary<char, Func<long, long, long>>
+        var stringOperations = new Dictionary<char, Func<long, long, string>>
+        {
+            ['|'] = (a, b) => $"{a}{b}",
+        };
+        var mathOperations = new Dictionary<char, Func<long, long, long>>
         {
             ['+'] = (a, b) => a + b,
             ['*'] = (a, b) => a * b,
@@ -69,6 +75,7 @@ public class Day07
             var stack = new Stack<string>();
             stack.Push("+");
             stack.Push("*");
+            stack.Push("|");
             var visited = new HashSet<string>();
             while (stack.Any())
             {
@@ -79,12 +86,53 @@ public class Day07
                 }
                 visited.Add(current);
 
-                if (current.Length == numbers.Count)
+                if (current.Length == numbers.Count - 1)
                 {
-                    var sum = numbers[0];
+                    var newNumbers = new List<long>();
+                    var queue = new Queue<long>();
                     for (var i = 1; i < numbers.Count; i++)
                     {
-                        sum = operations[current[i - 1]](sum, numbers[i]);
+                        if (current[i - 1] == '|')
+                        {
+                            queue.Enqueue(numbers[i - 1]);
+                        }
+                        else
+                        {
+                            if (queue.Any())
+                            {
+                                queue.Enqueue(numbers[i - 1]);
+                                var newNumber = new StringBuilder();
+                                while (queue.TryDequeue(out var x))
+                                {
+                                    newNumber.Append(x);
+                                }
+                                newNumbers.Add(long.Parse(newNumber.ToString()));
+                            }
+                            else
+                            {
+                                newNumbers.Add(numbers[i - 1]);
+                            }
+                        }
+                    }
+                    if (queue.Any())
+                    {
+                        queue.Enqueue(numbers[^1]);
+                        var newNumber = new StringBuilder();
+                        while (queue.TryDequeue(out var x))
+                        {
+                            newNumber.Append(x);
+                        }
+                        newNumbers.Add(long.Parse(newNumber.ToString()));
+                    }
+                    else
+                    {
+                        newNumbers.Add(numbers[^1]);
+                    }
+                    var newOperations = current.Where(x => x != '|').ToList();
+                    var sum = newNumbers[0];
+                    for (var i = 1; i < newNumbers.Count; i++)
+                    {
+                        sum += mathOperations[newOperations[i - 1]](sum, newNumbers[i]);
                     }
                     if (sum == answer)
                     {
@@ -96,6 +144,7 @@ public class Day07
 
                 stack.Push(current + "+");
                 stack.Push(current + "*");
+                stack.Push(current + "|");
             }
         }
         return result.ToString();
@@ -116,31 +165,29 @@ public class Day07
             292: 11 6 16 20
             """;
         var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
-    }
-    
-    [TestMethod]
-    public void Day07_Part1_Example02()
-    {
-        var input = """
-            <TODO>
-            """;
-        var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("3749", result);
     }
     
     [TestMethod]
     public void Day07_Part1()
     {
         var result = Part1(Common.DayInput(nameof(Day07), "2024"));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("3119088655389", result);
     }
     
     [TestMethod]
     public void Day07_Part2_Example01()
     {
         var input = """
-            <TODO>
+            190: 10 19
+            3267: 81 40 27
+            83: 17 5
+            156: 15 6
+            7290: 6 8 6 15
+            161011: 16 10 13
+            192: 17 8 14
+            21037: 9 7 18 13
+            292: 11 6 16 20
             """;
         var result = Part2(Common.GetLines(input));
         Assert.AreEqual("", result);
