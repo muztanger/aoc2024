58256232a94f2a252a2508ce78c2686c122a0fd9
Johan Östrand <muztanger@gmail.com>
2024-12-08 07:54:29 +0100

    Day 8 finished

--- Aoc2024/Day08.cs
+++ Aoc2024/Day08.cs
@@ -101,16 +101,99 @@ public class Day08
 
         return antiNodes.Count.ToString();
     }
-    
+
     private static string Part2(IEnumerable<string> input)
     {
-        var result = new StringBuilder();
+        var antennas = new DefaultValueDictionary<char, List<Antenna>>(() => new List<Antenna>());
+        var box = new Box<int>(1, 1);
+        var row = 0;
+        var occupied = new HashSet<Pos<int>>();
         foreach (var line in input)
         {
+            if (string.IsNullOrEmpty(line))
+            {
+                continue;
+            }
+            for (var col = 0; col < line.Length; col++)
+            {
+                Pos<int> pos = new Pos<int>(col, row);
+
+                char frequency = line[col];
+                if (frequency != '.')
+                {
+                    antennas[frequency].Add(new Antenna()
+                    {
+                        Frequency = frequency,
+                        Pos = pos
+                    });
+                    occupied.Add(pos);
+                }
+                box.IncreaseToPoint(pos);
+            }
+            row++;
         }
-        return result.ToString();
+
+        var antiNodes = new HashSet<Pos<int>>();
+        foreach (var frequency in antennas.Keys)
+        {
+            foreach (var a1 in antennas[frequency])
+            {
+                foreach (var a2 in antennas[frequency])
+                {
+                    if (a1.Pos == a2.Pos)
+                    {
+                        continue;
+                    }
+
+                    var dp = a2.Pos - a1.Pos;
+                    List<Pos<int>> positions = [a1.Pos - dp, a2.Pos + dp];
+
+                    {
+                        var p = a1.Pos;
+                        while (box.Contains(p))
+                        {
+                            antiNodes.Add(p);
+                            p -= dp;
+                        }
+                    }
+                    {
+                        var p = a2.Pos;
+                        while (box.Contains(p))
+                        {
+                            antiNodes.Add(p);
+                            p += dp;
+                        }
+                    }
+                }
+            }
+        }
+
+        var result = new StringBuilder();
+        for (var y = 0; y <= box.Max.y; y++)
+        {
+            for (var x = 0; x <= box.Max.x; x++)
+            {
+                if (occupied.Contains(new Pos<int>(x, y)))
+                {
+                    result.Append('a');
+                }
+                else if (antiNodes.Contains(new Pos<int>(x, y)))
+                {
+                    result.Append('#');
+                }
+                else
+                {
+                    result.Append('.');
+                }
+            }
+            result.AppendLine();
+        }
+        Console.WriteLine(result.ToString());
+
+
+        return antiNodes.Count.ToString();
     }
-    
+
     [TestMethod]
     public void Day08_Part1_Example01()
     {
@@ -133,16 +216,6 @@ public class Day08
     }
     
     [TestMethod]
-    public void Day08_Part1_Example02()
-    {
-        var input = """
-            <TODO>
-            """;
-        var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
-    }
-    
-    [TestMethod]
     public void Day08_Part1()
     {
         var result = Part1(Common.DayInput(nameof(Day08), "2024"));
@@ -154,10 +227,21 @@ public class Day08
     public void Day08_Part2_Example01()
     {
         var input = """
-            <TODO>
+            ............
+            ........0...
+            .....0......
+            .......0....
+            ....0.......
+            ......A.....
+            ............
+            ............
+            ........A...
+            .........A..
+            ............
+            ............
             """;
         var result = Part2(Common.GetLines(input));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("34", result);
     }
     
     [TestMethod]
