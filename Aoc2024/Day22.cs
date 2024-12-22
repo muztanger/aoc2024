db758e043c80c11977b7d5872cb2c8e434b98472
Johan Östrand <muztanger@gmail.com>
2024-12-22 06:30:12 +0100

    Day22 part 1 cleanup

--- Aoc2024/Day22.cs
+++ Aoc2024/Day22.cs
@@ -12,34 +12,17 @@ public class Day22
         {
             var initialSecretNumber = long.Parse(line);
 
-            //To mix a value into the secret number, calculate the bitwise XOR of the given value and the secret number.
-            //Then, the secret number becomes the result of that operation. (If the secret number is 42 and you were to mix 15 into the secret number, the secret number would become 37.)
             long Mix(long value, long secretNumber) => value ^ secretNumber;
-
             Assert.AreEqual(37, Mix(42, 15));
 
-            //To prune the secret number, calculate the value of the secret number modulo 16777216.Then, the secret number becomes the result of that operation.
-            //(If the secret number is 100000000 and you were to prune the secret number, the secret number would become 16113920.)
             long Prune(long secretNumber) => secretNumber & 0xFFFFFF;
             Assert.AreEqual(16113920, Prune(100000000));
 
             long x = initialSecretNumber;
             for (long i = 0; i < 2000; i++)
             {
-                //Calculate the result of multiplying the secret number by 64.
-                //Then, mix this result into the secret number.
-                //Finally, prune the secret number.
                 x = Prune(Mix(x << 6, x));
-
-                //Calculate the result of dividing the secret number by 32.
-                //Round the result down to the nearest integer.
-                //Then, mix this result into the secret number.
-                //Finally, prune the secret number.
                 x = Prune(Mix(x >> 5, x));
-
-                //Calculate the result of multiplying the secret number by 2048.
-                //Then, mix this result into the secret number.
-                //Finally, prune the secret number.
                 x = Prune(Mix(x << 11, x));
                 //Console.WriteLine($"{initialSecretNumber}: {x}");
             }
@@ -77,14 +60,14 @@ public class Day22
             123
             """;
         var result = Part1(Common.GetLines(input));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("1110806", result); // For consistency
     }
     
     [TestMethod]
     public void Day22_Part1()
     {
         var result = Part1(Common.DayInput(nameof(Day22), "2024"));
-        Assert.AreEqual("", result);
+        Assert.AreEqual("17163502021", result);
     }
     
     [TestMethod]
